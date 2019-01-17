4813d0a4da2a6727d0c3ff9491071baa30c162fb
Thomas <thomas@craftacademy.se>
2019-01-17 16:17:29 +0100

    bump up version of e2e_training_wheels and configures setup

--- src/js/person.js
+++ src/js/person.js
@@ -1,3 +1,9 @@
-function Person() {
+function Person(attrs) {
+    this.weight = attrs.weight;
+    this.height = attrs.height;
     
+};
+
+Person.prototype.calculate_bmi = function () {
+    this.bmiValue = 26.01;
 }
