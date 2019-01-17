b268c7d6df0027bb8f45947f889e30019d9de17d
Thomas <thomas@craftacademy.se>
2019-01-17 21:19:54 +0100

    refactors js: moves away from using prototype

--- src/js/bmi_calculator.js
+++ src/js/bmi_calculator.js
@@ -1,31 +1,30 @@
 function BMICalculator() {
+    this.metric_bmi = function (object) {
+        const weight = object.weight;
+        const height = object.height;
+        if (weight > 0 && height > 0) {
+            const finalBmi = weight / (height / 100 * height / 100);
+            object.bmiValue = parseFloat(finalBmi.toFixed(2));
+            this.setBMIMessage(object)
+        }
+    
+    }
+
+    this.setBMIMessage = function(object) {
+        if (object.bmiValue < 18.5) {
+            object.bmiMessage = "Underweight"
+        }
+        if (object.bmiValue > 18.5 && object.bmiValue < 25) {
+            object.bmiMessage = "Healthy"
+        }
+        if (object.bmiValue > 25 && object.bmiValue < 30) {
+            object.bmiMessage = "Overweight"
+        }
+        if (object.bmiValue > 30) {
+            object.bmiMessage = "Obese"
+        }
+    }
 };
-
-BMICalculator.prototype.metric_bmi = function (object) {
-    const weight = object.weight;
-    const height = object.height;
-    if (weight > 0 && height > 0) {
-        const finalBmi = weight / (height / 100 * height / 100);
-        object.bmiValue = parseFloat(finalBmi.toFixed(2));
-        setBMIMessage(object)
-    }
-
-}
-
-function setBMIMessage(object) {
-    if (object.bmiValue < 18.5) {
-        object.bmiMessage = "Underweight"
-    }
-    if (object.bmiValue > 18.5 && object.bmiValue < 25) {
-        object.bmiMessage = "Healthy"
-    }
-    if (object.bmiValue > 25 && object.bmiValue < 30) {
-        object.bmiMessage = "Overweight"
-    }
-    if (object.bmiValue > 30) {
-        object.bmiMessage = "Obese"
-    }
-}
 
 
 if (typeof module !== 'undefined' && module.exports) {
--- src/js/person.js
+++ src/js/person.js
@@ -1,15 +1,11 @@
 function Person(attrs) {
     this.weight = attrs.weight;
     this.height = attrs.height;
-    
+    this.calculate_bmi = function () {
+        const calculator = new BMICalculator();
+        calculator.metric_bmi(this)
+    }
 };
-
-Person.prototype.calculate_bmi = function () {
-    // this.bmiValue = 26.01;
-    // this.bmiMessage = 'Overweight'
-    const calculator = new BMICalculator();
-    calculator.metric_bmi(this)
-}
 
 if (typeof module !== 'undefined' && module.exports) {
     module.exports = Person;
